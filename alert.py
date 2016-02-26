ca874f736dbdaaf546b210bcca3bab4ff1380bf7
zkan <kan@prontomarketing.com>
2016-02-26 08:29:18 +0700

    Add dummy example

--- alert.py
+++ alert.py
@@ -1,18 +1,22 @@
+import socket
 import smtplib
 
-class EmailSending: 
-	def send_message(self, users, message):
-		smtpobj = smtplib.SMTP('localhost', 1025)
-		status = smtpobj.sendmail('user', users, message)
-		smtpobj.quit()
 
+class EmailSending:
+    def send_message(self, users, message):
+        try:
+            smtpobj = smtplib.SMTP('localhost', 1025)
+            smtpobj.sendmail('user', users, message)
+            smtpobj.quit()
+            return True
+        except socket.error:
+            return False
 
-	def say_in_email(self, name, recipient):
-		hellomessage = self.hello(name) + ". You are " + self.hello(name)
-		message = 'Ah ' + hellomessage
-		self.send_message(recipient, message)
-		return message
+    def say_in_email(self, name, recipient):
+        hellomessage = self.hello(name) + '. You are ' + self.hello(name)
+        message = 'Ah ' + hellomessage
+        self.send_message(recipient, message)
+        return message
 
-	def hello(self, name):
-		return "hello %s" % name
-
+    def hello(self, name):
+        return "hello %s" % name
