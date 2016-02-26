02541419e5cfa313b0beadf6f5e777a56b835371
zkan <kan@prontomarketing.com>
2016-02-26 09:14:53 +0700

    Revise stub examples

--- alert.py
+++ alert.py
@@ -15,8 +15,10 @@ class EmailSender:
     def say_in_email(self, name, recipient):
         hellomessage = self.hello(name) + '. You are ' + self.hello(name)
         message = 'Ah ' + hellomessage
-        self.send_message(recipient, message)
-        return message
+        if self.send_message(recipient, message):
+            return message
+        else:
+            return 'Error in sending email'
 
     def hello(self, name):
         return "hello %s" % name
--- test_mock.py
+++ test_mock.py
@@ -38,8 +38,16 @@ class TestMockSendEmail(unittest.TestCase):
         self.assertEqual(self.email_sender.hello.call_count, 2)
 
     def test_stub(self):
-        with patch("smtplib.SMTP") as mock_smtp:
-            self.email_sender.send_message('natty', 'hello')
+        with patch('alert.EmailSender.send_message') as mock_send_message:
+            mock_send_message.return_value = True
+            message = self.email_sender.say_in_email('natty', 'hello')
+            self.assertEqual(message, 'Ah hello natty. You are hello natty')
+
+    def test_another_stub(self):
+        with patch('alert.EmailSender.send_message') as mock_send_message:
+            mock_send_message.return_value = False
+            message = self.email_sender.say_in_email('natty', 'hello')
+            self.assertEqual(message, 'Error in sending email')
 
     def test_dummy(self):
         success = self.email_sender.send_message(MagicMock(), MagicMock())
